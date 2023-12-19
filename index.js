cf03887f9f2e3dea9d76e2e7311f6a7de722d265
anasachu2000 <muhammedanasea2000@gmail.com>
2023-12-19 09:37:29 +0530

    limit increased

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const app = express();
 env.config();
 mongoose.connect(process.env.mongo_url);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit:'10mb'}));
+app.use(express.urlencoded({limit:'10mb', extended: true }));
 
 app.use(
   cors({
